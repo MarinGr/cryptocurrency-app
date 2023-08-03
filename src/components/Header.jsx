dd01270f73d911b138f853c59fb33582f67e2072
Marina Gr <marina.grishn@gmail.com>
2023-08-03 23:37:31 +0300

    replace route link to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,17 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <StyledLink to="/cryptocurrency-app">
-      <Container>CR</Container>
-    </StyledLink>
+    <Container>
+      <StyledLink to="/cryptocurrency-app">
+        <Logo> CR</Logo>
+      </StyledLink>
+    </Container>
   );
 }
 
 const StyledLink = styled(Link)`
   text-decoration: none;
+  color: inherit;
 `;
 
 const Container = styled.div`
@@ -18,6 +21,9 @@ const Container = styled.div`
   background-color: var(--header-bg-color);
   color: var(--primary-text-color);
   padding: 1rem 3rem;
+`;
+
+const Logo = styled.p`
   font-family: "Oswald", sans-serif;
   font-size: 32px;
   font-weight: 600;
